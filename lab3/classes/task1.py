68df52e62a3cafed9b0faf0ca1d16d648db410b1
kilimanjarooo <oyyanqazaq@mail.ru>
2023-02-10 21:13:06 +0600

    lab 3

--- lab3/classes/task1.py
+++ lab3/classes/task1.py
@@ -2,7 +2,7 @@ class string:
   def getString(self):
     self.s = str(input())
   def printString(self):
-    print(self.s.upper())\
+    print(self.s.upper())
 
 answer = string()
 answer.getString()
